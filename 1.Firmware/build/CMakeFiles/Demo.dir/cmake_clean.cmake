2f233ac55542ff7bc4309c132d841195964a02f5
wzx <wangmumuyayaya@qq.com>
2021-08-09 10:41:22 +0100

    edited /linux-draw/DrawImage.cpp

--- 1.Firmware/build/CMakeFiles/Demo.dir/cmake_clean.cmake
+++ 1.Firmware/build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -3,6 +3,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/Demo.dir/linux-draw/readjpg.cpp.o"
   "CMakeFiles/Demo.dir/source/main.cpp.o"
   "CMakeFiles/Demo.dir/source/my_csv.cpp.o"
+  "CMakeFiles/Demo.dir/source/ring_queue.cpp.o"
   "Demo"
   "Demo.pdb"
 )
