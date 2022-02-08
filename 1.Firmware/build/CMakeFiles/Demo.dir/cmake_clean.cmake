467af5df97bd1786979da694f5e08d8586af5c3a
王木木 <2011647169@qq.com>
2022-02-08 20:57:54 +0800

    创建windows平台分支

--- 1.Firmware/build/CMakeFiles/Demo.dir/cmake_clean.cmake
+++ 1.Firmware/build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -1,11 +1,14 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Demo.dir/linux-draw/DrawImage.cpp.o"
-  "CMakeFiles/Demo.dir/linux-draw/readjpg.cpp.o"
-  "CMakeFiles/Demo.dir/source/main.cpp.o"
-  "CMakeFiles/Demo.dir/source/my_csv.cpp.o"
-  "CMakeFiles/Demo.dir/source/ring_queue.cpp.o"
-  "Demo"
+  "CMakeFiles/Demo.dir/linux-draw/DrawImage.cpp.obj"
+  "CMakeFiles/Demo.dir/linux-draw/readjpg.cpp.obj"
+  "CMakeFiles/Demo.dir/linux-draw/test.cpp.obj"
+  "CMakeFiles/Demo.dir/source/main.cpp.obj"
+  "CMakeFiles/Demo.dir/source/my_csv.cpp.obj"
+  "CMakeFiles/Demo.dir/source/ring_queue.cpp.obj"
+  "Demo.exe"
+  "Demo.exe.manifest"
   "Demo.pdb"
+  "libDemo.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
