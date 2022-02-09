a1fc13447489f93203f6158775f38abe469a4e2c
王木木 <2011647169@qq.com>
2022-02-09 15:42:15 +0800

    add .gitignore

---
+++ 1.Firmware/build/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/source/main.cpp" "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/build/CMakeFiles/Demo.dir/source/main.obj"
+  "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/source/my_csv.cpp" "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/build/CMakeFiles/Demo.dir/source/my_csv.obj"
+  "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/source/ring_queue.cpp" "C:/Users/WangZX/Desktop/test/RaspberryPi_FaceRecognition/1.Firmware/build/CMakeFiles/Demo.dir/source/ring_queue.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "F:/mingw64/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1.Firmware/build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/source/main.obj"
+  "CMakeFiles/Demo.dir/source/my_csv.obj"
+  "CMakeFiles/Demo.dir/source/ring_queue.obj"
+  "Demo.exe"
+  "Demo.exe.manifest"
+  "Demo.pdb"
+  "libDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
